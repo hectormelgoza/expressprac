1d1b372b4662a9041ed7213bde8e7a387d891c75
Hector Melgoza <h.melgoza.4@gmail.com>
2020-04-07 23:35:23 -0700

    updates

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+const cors = require('cors');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
-const msg = () => {
-console.log('Port is listening')
-}
+const uri = "mongodb+srv://hectormelgoza:thetrailblaze@cluster0-h4g1e.mongodb.net/test?retryWrites=true&w=majority";
+
+/* const connection = mongoose.connection;
+
+connection.once('open', () => {console.log('MongoDB connected')}) */
+const msg = () => {console.log('Port is listening')}
 
 const server = app.listen(3000, msg);
 
 app.get('/', (req, res) => {
-    res.send('hyyy');
+    res.send('type');
 })
+
