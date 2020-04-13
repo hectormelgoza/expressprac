0fe101c777dd00d58dd743373c87ad63aea0c30d
Hector Melgoza <h.melgoza.4@gmail.com>
2020-04-12 22:58:53 -0700

    connection to mongo

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
-const uri = "mongodb+srv://hectormelgoza:thetrailblaze@cluster0-h4g1e.mongodb.net/test?retryWrites=true&w=majority";
-
-/* const connection = mongoose.connection;
-
-connection.once('open', () => {console.log('MongoDB connected')}) */
 const msg = () => {console.log('Port is listening')}
 
 const server = app.listen(3000, msg);
-
 app.get('/', (req, res) => {
     res.send('type');
-})
+});
+
+const uri = "mongodb+srv://hectormelgoza:thetrailblaze@cluster0-h4g1e.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
+
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {console.log('MongoDB connected')})
+
+
 
